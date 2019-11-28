127608e211c6d94f897a912aa3feca9652fa36b2
alyph <chengjialeph@gmail.com>
2019-11-27 22:46:23 -0800

    use vswhere to find vs build path - instead of hard coding the path

--- rogue/build.bat
+++ rogue/build.bat
@@ -1,7 +1,20 @@
 @echo off
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64     
-
 pushd %~dp0
+
+for /f "usebackq tokens=*" %%i in (`.\external\msvc\vswhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+	set InstallDir=%%i
+)
+
+if not exist "%InstallDir%\VC\Auxiliary\Build\vcvarsall.bat" (
+	echo Error: invalid VS installation "%InstallDir%"
+	exit /b 1
+)
+
+echo Building using VS installation: %InstallDir%
+echo.
+
+call "%InstallDir%\VC\Auxiliary\Build\vcvarsall.bat" x64
+
 make %*
 popd
